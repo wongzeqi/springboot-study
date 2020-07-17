b46041a04095699fd7aa6cef63909ddb113efac4
wangjiaqi72 <Wzq960915@>
2020-07-17 17:31:40 +0800

    init project

---
+++ wjq-study/src/main/java/com/jd/wjq/WjqStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jd.wjq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjqStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjqStudyApplication.class, args);
+	}
+
+}
---
+++ wjq-study/src/test/java/com/jd/wjq/WjqStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.wjq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjqStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
